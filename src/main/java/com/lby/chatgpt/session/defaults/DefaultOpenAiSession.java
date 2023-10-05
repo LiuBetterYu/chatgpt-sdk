2f53a3ac27c48b9f3dd862a78d776d7b2051cefa
lby <2283165894@qq.com>
2023-10-05 10:07:10 +0800

    chatgpt-sdk--完善各类接口

--- src/main/java/com/lby/chatgpt/session/defaults/DefaultOpenAiSession.java
+++ src/main/java/com/lby/chatgpt/session/defaults/DefaultOpenAiSession.java
@@ -1,22 +1,44 @@
 package com.lby.chatgpt.session.defaults;
 
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.http.ContentType;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lby.chatgpt.IOpenAiApi;
+import com.lby.chatgpt.domain.billing.BillingUsage;
+import com.lby.chatgpt.domain.billing.Subscription;
 import com.lby.chatgpt.domain.chat.ChatCompletionRequest;
 import com.lby.chatgpt.domain.chat.ChatCompletionResponse;
+import com.lby.chatgpt.domain.edits.EditRequest;
+import com.lby.chatgpt.domain.edits.EditResponse;
+import com.lby.chatgpt.domain.embedd.EmbeddingRequest;
+import com.lby.chatgpt.domain.embedd.EmbeddingResponse;
+import com.lby.chatgpt.domain.files.DeleteFileResponse;
+import com.lby.chatgpt.domain.files.UploadFileResponse;
+import com.lby.chatgpt.domain.images.ImageEditRequest;
+import com.lby.chatgpt.domain.images.ImageRequest;
+import com.lby.chatgpt.domain.images.ImageResponse;
+import com.lby.chatgpt.domain.other.OpenAiResponse;
 import com.lby.chatgpt.domain.qa.QACompletionRequest;
 import com.lby.chatgpt.domain.qa.QACompletionResponse;
+import com.lby.chatgpt.domain.whisper.TranscriptionsRequest;
+import com.lby.chatgpt.domain.whisper.TranslationsRequest;
+import com.lby.chatgpt.domain.whisper.WhisperResponse;
 import com.lby.chatgpt.session.Configuration;
 import com.lby.chatgpt.session.OpenAiSession;
 import io.reactivex.Single;
 import lombok.var;
 import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.sse.EventSource;
 import okhttp3.sse.EventSourceListener;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.time.LocalDate;
+import java.util.*;
 
 /**
  * @author lby
@@ -36,7 +58,7 @@ public class DefaultOpenAiSession implements OpenAiSession {
     }
 
     @Override
-    public QACompletionResponse completion(QACompletionRequest qaCompletionRequest) {
+    public QACompletionResponse completions(QACompletionRequest qaCompletionRequest) {
         return this.openAiApi.completions(qaCompletionRequest).blockingGet();
     }
 
@@ -89,5 +111,157 @@ public class DefaultOpenAiSession implements OpenAiSession {
 
         // 返回结果信息；EventSource 对象可以取消应答
         return factory.newEventSource(request, eventSourceListener);
+    }
+
+    @Override
+    public EditResponse edit(EditRequest editRequest) {
+        return this.openAiApi.edits(editRequest).blockingGet();
+    }
+
+    @Override
+    public ImageResponse genImages(String prompt) {
+        ImageRequest imageRequest = ImageRequest.builder().prompt(prompt).build();
+        return this.genImages(imageRequest);
+    }
+
+    @Override
+    public ImageResponse genImages(ImageRequest imageRequest) {
+        return this.openAiApi.genImages(imageRequest).blockingGet();
+    }
+
+    @Override
+    public ImageResponse editImages(File image, String prompt) {
+        ImageEditRequest imageEditRequest = ImageEditRequest.builder().prompt(prompt).build();
+        return this.editImages(image, null, imageEditRequest);
+    }
+
+    @Override
+    public ImageResponse editImages(File image, ImageEditRequest imageEditRequest) {
+        return this.editImages(image, null, imageEditRequest);
+    }
+
+    @Override
+    public ImageResponse editImages(File image, File mask, ImageEditRequest imageEditRequest) {
+        // 1. imageMultipartBody
+        RequestBody imageBody = RequestBody.create(MediaType.parse("multipart/form-data"), image);
+        MultipartBody.Part imageMultipartBody = MultipartBody.Part.createFormData("image", image.getName(), imageBody);
+        // 2. maskMultipartBody
+        MultipartBody.Part maskMultipartBody = null;
+        if (Objects.nonNull(mask)) {
+            RequestBody maskBody = RequestBody.create(MediaType.parse("multipart/form-data"), mask);
+            maskMultipartBody = MultipartBody.Part.createFormData("mask", mask.getName(), maskBody);
+        }
+        // requestBodyMap
+        Map<String, RequestBody> requestBodyMap = new HashMap<>();
+        requestBodyMap.put("prompt", RequestBody.create(MediaType.parse("multipart/form-data"), imageEditRequest.getPrompt()));
+        requestBodyMap.put("n", RequestBody.create(MediaType.parse("multipart/form-data"), imageEditRequest.getN().toString()));
+        requestBodyMap.put("size", RequestBody.create(MediaType.parse("multipart/form-data"), imageEditRequest.getSize()));
+        requestBodyMap.put("response_format", RequestBody.create(MediaType.parse("multipart/form-data"), imageEditRequest.getResponseFormat()));
+        if (!(Objects.isNull(imageEditRequest.getUser()) || "".equals(imageEditRequest.getUser()))) {
+            requestBodyMap.put("user", RequestBody.create(MediaType.parse("multipart/form-data"), imageEditRequest.getUser()));
+        }
+        return this.openAiApi.editImages(imageMultipartBody, maskMultipartBody, requestBodyMap).blockingGet();
+    }
+
+    @Override
+    public EmbeddingResponse embeddings(String input) {
+        EmbeddingRequest embeddingRequest = EmbeddingRequest.builder().input(new ArrayList<String>() {{
+            add(input);
+        }}).build();
+        return this.embeddings(embeddingRequest);
+    }
+
+    @Override
+    public EmbeddingResponse embeddings(String... inputs) {
+        EmbeddingRequest embeddingRequest = EmbeddingRequest.builder().input(Arrays.asList(inputs)).build();
+        return this.embeddings(embeddingRequest);
+    }
+
+    @Override
+    public EmbeddingResponse embeddings(List<String> inputs) {
+        EmbeddingRequest embeddingRequest = EmbeddingRequest.builder().input(inputs).build();
+        return this.embeddings(embeddingRequest);
+    }
+
+    @Override
+    public EmbeddingResponse embeddings(EmbeddingRequest embeddingRequest) {
+        return this.openAiApi.embeddings(embeddingRequest).blockingGet();
+    }
+
+    @Override
+    public OpenAiResponse<File> files() {
+        return this.openAiApi.files().blockingGet();
+    }
+
+    @Override
+    public UploadFileResponse uploadFile(File file) {
+        return this.uploadFile("fine-tune", file);
+    }
+
+    @Override
+    public UploadFileResponse uploadFile(String purpose, File file) {
+        RequestBody fileBody = RequestBody.create(MediaType.parse("multipart/form-data"), file);
+        MultipartBody.Part multipartBody = MultipartBody.Part.createFormData("file", file.getName(), fileBody);
+        RequestBody purposeBody = RequestBody.create(MediaType.parse("multipart/form-data"), purpose);
+        return this.openAiApi.uploadFile(multipartBody, purposeBody).blockingGet();
+    }
+
+    @Override
+    public DeleteFileResponse deleteFile(String fileId) {
+        return this.openAiApi.deleteFile(fileId).blockingGet();
+    }
+
+    @Override
+    public WhisperResponse speed2TextTranscriptions(File file, TranscriptionsRequest transcriptionsRequest) {
+        // 1. 语音文件
+        RequestBody fileBody = RequestBody.create(MediaType.parse("multipart/form-data"), file);
+        MultipartBody.Part multipartBody = MultipartBody.Part.createFormData("file", file.getName(), fileBody);
+        // 2. 参数封装
+        Map<String, RequestBody> requestBodyMap = new HashMap<>();
+        if (StrUtil.isNotBlank(transcriptionsRequest.getLanguage())) {
+            requestBodyMap.put(TranscriptionsRequest.Fields.language, RequestBody.create(MediaType.parse("multipart/form-data"), transcriptionsRequest.getLanguage()));
+        }
+        if (StrUtil.isNotBlank(transcriptionsRequest.getModel())) {
+            requestBodyMap.put(TranscriptionsRequest.Fields.model, RequestBody.create(MediaType.parse("multipart/form-data"), transcriptionsRequest.getModel()));
+        }
+        if (StrUtil.isNotBlank(transcriptionsRequest.getPrompt())) {
+            requestBodyMap.put(TranscriptionsRequest.Fields.prompt, RequestBody.create(MediaType.parse("multipart/form-data"), transcriptionsRequest.getPrompt()));
+        }
+        if (StrUtil.isNotBlank(transcriptionsRequest.getResponseFormat())) {
+            requestBodyMap.put(TranscriptionsRequest.Fields.responseFormat, RequestBody.create(MediaType.parse("multipart/form-data"), transcriptionsRequest.getResponseFormat()));
+        }
+        requestBodyMap.put(TranscriptionsRequest.Fields.temperature, RequestBody.create(MediaType.parse("multipart/form-data"), String.valueOf(transcriptionsRequest.getTemperature())));
+        return this.openAiApi.speed2TextTranscriptions(multipartBody, requestBodyMap).blockingGet();
+    }
+
+    @Override
+    public WhisperResponse speed2TextTranslations(File file, TranslationsRequest translationsRequest) {
+        // 1. 语音文件
+        RequestBody fileBody = RequestBody.create(MediaType.parse("multipart/form-data"), file);
+        MultipartBody.Part multipartBody = MultipartBody.Part.createFormData("file", file.getName(), fileBody);
+        // 2. 参数封装
+        Map<String, RequestBody> requestBodyMap = new HashMap<>();
+        if (StrUtil.isNotBlank(translationsRequest.getModel())) {
+            requestBodyMap.put(TranslationsRequest.Fields.model, RequestBody.create(MediaType.parse("multipart/form-data"), translationsRequest.getModel()));
+        }
+        if (StrUtil.isNotBlank(translationsRequest.getPrompt())) {
+            requestBodyMap.put(TranslationsRequest.Fields.prompt, RequestBody.create(MediaType.parse("multipart/form-data"), translationsRequest.getPrompt()));
+        }
+        if (StrUtil.isNotBlank(translationsRequest.getResponseFormat())) {
+            requestBodyMap.put(TranslationsRequest.Fields.responseFormat, RequestBody.create(MediaType.parse("multipart/form-data"), translationsRequest.getResponseFormat()));
+        }
+        requestBodyMap.put(TranslationsRequest.Fields.temperature, RequestBody.create(MediaType.parse("multipart/form-data"), String.valueOf(translationsRequest.getTemperature())));
+        requestBodyMap.put(TranscriptionsRequest.Fields.temperature, RequestBody.create(MediaType.parse("multipart/form-data"), String.valueOf(translationsRequest.getTemperature())));
+        return this.openAiApi.speed2TextTranscriptions(multipartBody, requestBodyMap).blockingGet();
+    }
+
+    @Override
+    public Subscription subscription() {
+        return this.openAiApi.subscription().blockingGet();
+    }
+
+    @Override
+    public BillingUsage billingUsage(@NotNull LocalDate starDate, @NotNull LocalDate endDate) {
+        return this.openAiApi.billingUsage(starDate, endDate).blockingGet();
     }
 }
